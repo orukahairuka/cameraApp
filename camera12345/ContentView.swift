f062af45dabbd94886171a21cbdaaa320d636203
orukahairuka <sakuraierika03301109@gmail.com>
2024-03-27 22:44:27 +0900

    Initial Commit

---
+++ camera12345/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  camera12345
+//
+//  Created by 櫻井絵理香 on 2024/03/27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ camera12345/camera12345App.swift
@@ -0,0 +1,17 @@
+//
+//  camera12345App.swift
+//  camera12345
+//
+//  Created by 櫻井絵理香 on 2024/03/27.
+//
+
+import SwiftUI
+
+@main
+struct camera12345App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
